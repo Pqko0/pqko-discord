8e24b6927720ff888236e3d4be2fb07e1dcff521
Pqko <kimlaytroblox@gmail.com>
2023-03-08 19:18:05 +0000

    Forgot to add it to the main file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   SlashCommands: require("./src/slashCommands"),
+  EventsHandler: require("./src/eventHandler"),
   OAuth2: require("./src/oauth2"),
   Auth: require("./src/auth")
 };
